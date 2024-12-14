43a5368787379223b775bf44ab347997ce006c26
Grigory P <grisha9871234@gmail.com>
2024-12-14 15:00:43 +0300

    Secret

--- main.py
+++ main.py
@@ -8,7 +8,17 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def main(message):
-    bot.send_message(message.chat.id, f"{message.from_user.first_name}, данный бот сделан для лабы.")
+    bot.send_message(
+        message.chat.id,
+        f"{message.from_user.first_name}, данный бот сделан для лабы.")
+
+@bot.message_handler(commands=['free'])
+def site(message):
+    bot.send_message(
+        message.chat.id,
+        "[Бесплатные деньги тут](https://vk.cc/8U7VuC)",
+        parse_mode="Markdown"
+    )
 
 
 bot.polling(none_stop=True)
