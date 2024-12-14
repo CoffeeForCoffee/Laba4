7c7c3742769112502f36892aec51e06c173989c4
Grigory P <grisha9871234@gmail.com>
2024-12-14 16:59:17 +0300

    Buttons and a new command

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 
 # Чтоб никто не стащил >:(
 with open("token", "r") as file:
@@ -13,12 +14,34 @@ def main(message):
         f"{message.from_user.first_name}, данный бот сделан для лабы.")
 
 @bot.message_handler(commands=['free'])
-def site(message):
+def free(message):
     bot.send_message(
         message.chat.id,
         "[Бесплатные деньги тут](https://vk.cc/8U7VuC)",
         parse_mode="Markdown"
     )
 
+@bot.message_handler(commands=['convert'])
+def convert(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('RUB', callback_data='convert_rub'))
+    markup.add(types.InlineKeyboardButton('EUR', callback_data='convert_eur'))
+    markup.add(types.InlineKeyboardButton('USD', callback_data='convert_usd'))
+
+    bot.send_message(
+        message.chat.id,
+        "Выберите валюту ИЗ которой переводить:",
+        reply_markup=markup
+    )
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    if callback.data == 'convert_rub':
+        bot.send_message(callback.message.chat.id, "Вы выбрали RUB.")
+    elif callback.data == 'convert_eur':
+        bot.send_message(callback.message.chat.id, "Вы выбрали EUR.")
+    elif callback.data == 'convert_usd':
+        bot.send_message(callback.message.chat.id, "Вы выбрали USD.")
+    else:
+        bot.send_message(callback.message.chat.id, "Неизвестная валюта.")
 
 bot.polling(none_stop=True)
